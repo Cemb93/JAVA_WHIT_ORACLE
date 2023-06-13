01e82b676d035f5ba22130791e745d8cbea65f2c
Cemb93 <karloz13m@gmail.com>
2023-06-12 20:59:48 -0500

    Segundo comentario

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -4,6 +4,7 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String saludo = "hola mundo";
+		String segundo = "Segundo comentario";
 	}
 
 }
